b73d26e3be68c96fab43d98d633547c2633a3098
GaryStimson <20581382+GaryStimson@users.noreply.github.com>
2024-11-21 17:47:03 +0000

    Add files via upload
    Changed header

--- app/components/header/Header.tsx
+++ app/components/header/Header.tsx
@@ -4,8 +4,15 @@ import { chatStore } from '~/lib/stores/chat';
 import { classNames } from '~/utils/classNames';
 import { HeaderActionButtons } from './HeaderActionButtons.client';
 import { ChatDescription } from '~/lib/persistence/ChatDescription.client';
+import type { ChatHistoryItem } from '~/lib/persistence';
+import type { MetaFunction } from '@remix-run/cloudflare';
 
-export function Header() {
+interface HeaderProps {
+  onLogout?: MetaFunction;
+}
+
+
+export function Header({ onLogout }: HeaderProps) {
   const chat = useStore(chatStore);
 
   return (
@@ -18,10 +25,43 @@ export function Header() {
         },
       )}
     >
+      <div style={{ position: 'absolute', right: 10, top: 10, color: '#FFFFFF', cursor: 'pointer' }} onClick={()=>{
+        if ( typeof(onLogout) !== 'undefined' ) {
+          onLogout();
+        }
+      }}>
+        <svg fill="#FFFFFF" height="20px" width="20px" version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg"
+                       xmlns:xlink="http://www.w3.org/1999/xlink"
+                       viewBox="0 0 384.971 384.971" xml:space="preserve">
+          <g>
+            <g id="Sign_Out">
+              <path d="M180.455,360.91H24.061V24.061h156.394c6.641,0,12.03-5.39,12.03-12.03s-5.39-12.03-12.03-12.03H12.03
+                C5.39,0.001,0,5.39,0,12.031V372.94c0,6.641,5.39,12.03,12.03,12.03h168.424c6.641,0,12.03-5.39,12.03-12.03
+                C192.485,366.299,187.095,360.91,180.455,360.91z" />
+              <path d="M381.481,184.088l-83.009-84.2c-4.704-4.752-12.319-4.74-17.011,0c-4.704,4.74-4.704,12.439,0,17.179l62.558,63.46H96.279
+                c-6.641,0-12.03,5.438-12.03,12.151c0,6.713,5.39,12.151,12.03,12.151h247.74l-62.558,63.46c-4.704,4.752-4.704,12.439,0,17.179
+                c4.704,4.752,12.319,4.752,17.011,0l82.997-84.2C386.113,196.588,386.161,188.756,381.481,184.088z" />
+            </g>
+            <g>
+            </g>
+            <g>
+            </g>
+            <g>
+            </g>
+            <g>
+            </g>
+            <g>
+            </g>
+            <g>
+            </g>
+          </g>
+        </svg>
+      </div>
       <div className="flex items-center gap-2 z-logo text-bolt-elements-textPrimary cursor-pointer">
         <div className="i-ph:sidebar-simple-duotone text-xl" />
         <a href="/" className="text-2xl font-semibold text-accent flex items-center">
-          <span className="i-bolt:logo-text?mask w-[46px] inline-block" />
+          <span style={{ height: '3em', width: '3em', marginTop: -10 }}
+                className="i-bolt:logo-text?mask w-[46px] inline-block" />
         </a>
       </div>
       <span className="flex-1 px-4 truncate text-center text-bolt-elements-textPrimary">
@@ -37,5 +77,5 @@ export function Header() {
         </ClientOnly>
       )}
     </header>
-  );
+);
 }
