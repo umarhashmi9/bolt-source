ab941afd606b6ea789cfff4ff9234cd1b24b2039
GaryStimson <20581382+GaryStimson@users.noreply.github.com>
2024-11-21 17:48:26 +0000

    Add files via upload
    UI modified

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -3,9 +3,10 @@ import { ClientOnly } from 'remix-utils/client-only';
 import { BaseChat } from '~/components/chat/BaseChat';
 import { Chat } from '~/components/chat/Chat.client';
 import { Header } from '~/components/header/Header';
+import { Login } from '~/components/ui/Login';
 
 export const meta: MetaFunction = () => {
-  return [{ title: 'Bolt' }, { name: 'description', content: 'Talk with Bolt, an AI assistant from StackBlitz' }];
+  return [{ title: 'Gizmo Coder (Bolt)' }, { name: 'description', content: 'Talk with Gizmo, an AI assistant from LabVantage (powered by Bolt)' }];
 };
 
 export const loader = () => json({});
@@ -13,8 +14,7 @@ export const loader = () => json({});
 export default function Index() {
   return (
     <div className="flex flex-col h-full w-full">
-      <Header />
-      <ClientOnly fallback={<BaseChat />}>{() => <Chat />}</ClientOnly>
+      <Login>{( logout: MetaFunction)=><><Header onLogout={logout} /><ClientOnly fallback={<BaseChat />}>{() => <Chat />}</ClientOnly></>}</Login>
     </div>
   );
 }
