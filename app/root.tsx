9f59833c0d79492f630bf2deb1e772fbc547bbc8
fahadpatel-nc <fahad.patel@nymcard.com>
2024-12-18 15:04:05 +0400

    Updating

--- app/root.tsx
+++ app/root.tsx
@@ -16,7 +16,7 @@ import 'virtual:uno.css';
 export const links: LinksFunction = () => [
   {
     rel: 'icon',
-    href: '/favicon.svg',
+    href: '/icon.svg',
     type: 'image/svg+xml',
   },
   { rel: 'stylesheet', href: reactToastifyStyles },
