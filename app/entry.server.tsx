df1f1e32abc7fd10ef6236c440f02426dae8e387
Nercio <96576338+KelvinCelso@users.noreply.github.com>
2025-01-10 19:02:15 +0200

    commit

--- app/entry.server.tsx
+++ app/entry.server.tsx
@@ -1,7 +1,8 @@
 import type { AppLoadContext, EntryContext } from '@remix-run/cloudflare';
+// @ts-expect-error
+import { renderToReadableStream } from 'react-dom/server.browser';
 import { RemixServer } from '@remix-run/react';
 import { isbot } from 'isbot';
-import { renderToReadableStream } from 'react-dom/server';
 import { renderHeadToString } from 'remix-island';
 import { Head } from './root';
 import { themeStore } from '~/lib/stores/theme';
