75cc1c837dc4ee914e27079b3f00af631de889cd
kakmul <46395013+kakmul@users.noreply.github.com>
2025-01-15 01:58:24 +0700

    Update _index.tsx
    change meta

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -6,7 +6,7 @@ import { Header } from '~/components/header/Header';
 import BackgroundRays from '~/components/ui/BackgroundRays';
 
 export const meta: MetaFunction = () => {
-  return [{ title: 'Bolt' }, { name: 'description', content: 'Talk with Bolt, an AI assistant from StackBlitz' }];
+  return [{ title: 'Bikinkode' }, { name: 'description', content: 'Platform AI untuk membuat website dengan cepat dan mudah. Tanpa coding, cukup masukkan kebutuhan Anda, dan website profesional siap dalam hitungan menit. Coba sekarang!' }];
 };
 
 export const loader = () => json({});
