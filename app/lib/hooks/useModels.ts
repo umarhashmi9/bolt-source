efcb459c431eb77313da8057fc0dd800e574da5c
Oliver Jägle <github@beimir.net>
2025-01-16 13:10:17 +0100

    Remove obsolete artifact

--- app/lib/hooks/useModels.ts
+++
@@ -1,7 +0,0 @@
-import { useLoaderData } from '@remix-run/react';
-import type { ModelInfo } from '~/lib/modules/llm/types';
-
-export function useModels() {
-  const { modelList } = useLoaderData<{ modelList: ModelInfo[] }>();
-  return modelList;
-}
