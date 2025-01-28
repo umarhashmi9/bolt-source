c3c1400e4b5062b1a7fe11662b23b9cd4cb036f7
book311 <alaabook1789@gmail.com>
2025-01-28 03:49:53 +0300

    Update BaseChat.tsx

--- app/components/chat/BaseChat.tsx
+++ app/components/chat/BaseChat.tsx
@@ -296,10 +296,10 @@ export const BaseChat = React.forwardRef<HTMLDivElement, BaseChatProps>(
             {!chatStarted && (
               <div id="intro" className="mt-[16vh] max-w-chat mx-auto text-center px-4 lg:px-0">
                 <h1 className="text-3xl lg:text-6xl font-bold text-bolt-elements-textPrimary mb-4 animate-fade-in">
-                  Where ideas begin
+                  Welcome to Dlicom AI
                 </h1>
                 <p className="text-md lg:text-xl mb-8 text-bolt-elements-textSecondary animate-fade-in animation-delay-200">
-                  Bring ideas to life in seconds or get help on existing projects.
+                  Bring ideas to life in with Dlicom AI.
                 </p>
               </div>
             )}
