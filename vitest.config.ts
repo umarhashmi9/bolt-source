496f54f25f8280cb897d65715ebed3f7fce2346a
Dustin Loring <dustinwloring1988@gmail.com>
2025-01-28 05:31:44 -0500

    Create vitest.config.ts
    exclude e2e and a few other directories from tests

---
+++ vitest.config.ts
@@ -0,0 +1,14 @@
+import { defineConfig } from 'vitest/config';
+
+export default defineConfig({
+  test: {
+    exclude: [
+      '**/node_modules/**',
+      '**/dist/**',
+      '**/e2e/**',
+      '.idea',
+      '.git',
+      '.cache'
+    ],
+  },
+}); 
