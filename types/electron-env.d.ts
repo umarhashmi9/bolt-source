8aca76b0d97da5feed06e062f081e038542764bf
jiewanle123 <jiewanle123@gmail.com>
2025-03-30 18:02:55 +0800

    Revert "ci(prettier):  Add Prettier workflow and update configuration file"
    This reverts commit 9b317c594c4ff323dae4f15718e2611314a72ccc.

--- types/electron-env.d.ts
+++ types/electron-env.d.ts
@@ -4,28 +4,28 @@ interface Window {
      * Get system locale settings
      */
     getSystemLocale: () => Promise<string>;
-
+    
     /**
      * Set language
      * @param language language code
      */
     setLanguage: (language: string) => Promise<boolean>;
-
+    
     /**
      * Get stored language settings
      */
     getLanguage: () => Promise<string | undefined>;
   };
-
+  
   ipc?: {
     /**
      * Invoke main process method
      */
     invoke: (...args: any[]) => Promise<any>;
-
+    
     /**
      * Listen to main process events
      */
-    on: (channel: string, func: Function) => () => void;
+    on: (channel: string, func: Function) => (() => void);
   };
 }
